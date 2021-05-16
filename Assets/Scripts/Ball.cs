38c69b7450c216de6420e87d463bcacb3c4435c5
Adrian Furmanek <adrian.furm@gmail.com>
2021-05-17 00:42:06 +0200

    Some ball physics and block collision

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
-
+    bool hasStarted = false;
     Vector2 paddleToBallVector;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+       
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LunchOnMouseClick();
+        }
+       
+        
+    }
+
+    private void LunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
