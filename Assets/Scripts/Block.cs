5b17fba2d250c33bd2a3c24d82c616d8b3ef9158
Adrian Furmanek <adrian.furm@gmail.com>
2021-05-21 01:33:11 +0200

    VFX and State
    VFX effect for destroyed blocks and block state at one and two hits

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,25 +5,76 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     Level level;
-   
+
+    //state
+    [SerializeField] int timesHit; // serialized for debug
 
     private void Start()
     {
-        level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        CountBreakableBlocks();
     }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if(tag == "Breakable")
+        {
+            HandleHit();
+
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
     {
+            PlayBlockDestroySFX();
+            Destroy(gameObject);
+            level.BlockDestroyed();
+            TriggerSparklesVFX();
+   
+    }
+
+    private void PlayBlockDestroySFX()
+    {
         FindObjectOfType<GameStatus>().AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(gameObject);
-        level.BlockDestroyed();
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -12,7 +12,7 @@ public class Level : MonoBehaviour
     {
         sceneloader = FindObjectOfType<SceneLoader>();
     }
-    public void CountBreakableBlocks()
+    public void CountBlocks()
     {
         breakableBlocks++;
     }
